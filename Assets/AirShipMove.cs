15e5296f67a10f28f409f96360a870686dc7c94a
rahulstejwani <115567486+rahulstejwani@users.noreply.github.com>
2023-05-09 12:28:18 -0400

    Added airships and fixed lighting and camera

---
+++ Assets/AirShipMove.cs
@@ -0,0 +1,38 @@
+using System.Numerics;
+using UnityEngine;
+
+public class AirShipMove : MonoBehaviour
+{
+    public float distance = 5f; // Distance to move
+    public float speed = 2f; // Speed of movement
+    private bool movingForward = true; // Flag to indicate if moving left or right
+    private UnityEngine.Vector3 startPos; // Starting position of the sprite
+
+    void Start()
+    {
+        startPos = transform.position; // Save the starting position of the sprite
+    }
+
+    void Update()
+    {
+        if (movingForward)
+        {
+            transform.Translate(UnityEngine.Vector3.left * speed * Time.deltaTime); // Move left
+
+            // Check if the object has moved the specified distance
+            if (transform.position.x < startPos.x - distance)
+            {
+                // If it has, teleport the object back to its starting position and set the flag to start moving forward again
+                transform.position = startPos;
+                movingForward = true;
+            }
+        }
+        else
+        {
+            transform.position = startPos;
+
+            // Set the flag to start moving forward again
+            movingForward = true;
+        }
+    }
+}
